e98b1e51b0138d30d53fdff65bd6c8f154673db1
abdul2020 <git.generalassemb.ly>
2020-04-12 05:37:46 +0300

    updates-js-and-css

--- script/main.js
+++ script/main.js
@@ -6,57 +6,93 @@ const objDOMs = {
 	listContainer: document.getElementById('list-container'),
 	dataInCircle: document.getElementById('data-in-circle'),
 	TimeInCircle: document.querySelector('.time-in-circle'),
+};
+
+const status = {
+	state: false,
 	id: 0,
 };
 
 console.log(objDOMs.TimeInCircle);
+// console.log(localStorage.setItem('time', objDOMs.TimeInCircle.innerHTML));
 
-function createTasks() {
-	let html = `<div class="list-${objDOMs.id}">
-    <div class="info">
-        <div class="info-input">
-            <input class="checkbox"  type="checkbox" />
-            <h3 class="incomingValue">${objDOMs.input.value}</h3>
-            <div class="time">
-                <i class="fas fa-clock"> ${date('list')}</i>
-            </div>
-        </div>
-        <button class="delBtn"><i class="fas fa-trash del"></i></button>
-    </div>  
-</div>`;
-	objDOMs.listContainer.insertAdjacentHTML('afterbegin', html);
-	objDOMs.id += 1;
+function createTasks(e) {
+	// // console.log(e.keyCode);
+	// console.log(e.type);
+	if (e.keyCode === 13 || e.type === 'click') {
+		let html = `<div class="list-${status.id}">
+		<div class="info" status="${status.state}">
+			<div class="info-input">
+				<input class="checkbox"  type="checkbox" />
+				<h3 class="incomingValue">${objDOMs.input.value}</h3>
+				<div class="time">
+					<i class="fas fa-clock"> ${date('list')}</i>
+				</div>
+			</div>
+			<button class="delBtn"><i class="fas fa-trash del"></i></button>
+		</div>  
+	</div>`;
+		objDOMs.listContainer.insertAdjacentHTML('afterbegin', html);
+		status.id += 1;
+		objDOMs.input.value = ' ';
+		objDOMs.input.focus();
+	}
 }
 
 function checkedTasked(event) {
 	if (event.target.className === 'checkbox') {
+		//access the second parent and change the  checked status of it
+		if (event.target.parentNode.parentNode.getAttribute('status') === 'false') {
+			event.target.parentNode.parentNode.setAttribute('status', 'true');
+		} else {
+			event.target.parentNode.parentNode.setAttribute('status', 'false');
+		}
 		let u = event.target.nextElementSibling;
 		let parent = u.parentNode.parentNode;
 		u.classList.toggle('checked');
 		parent.classList.toggle('checked-list');
+		// console.log(event.target.parentNode.parentNode);
+
+		//here access the next sibling which is the input and toggle a class to it
 	}
 }
 
 function deleteTasks(event) {
 	if (event.target.className === 'delBtn') {
-		objDOMs.listContainer.removeChild(event.target.parentNode.parentNode);
+		//check if the status of checkedbox is true
+		if (event.target.parentNode.getAttribute('status') === 'true') {
+			objDOMs.listContainer.removeChild(event.target.parentNode.parentNode);
+		} else {
+			// need improvment
+
+			event.target.parentNode.classList.toggle('shake');
+			console.log(event.target.parentNode);
+		}
 	}
 }
 
-objDOMs.dataInCircle.textContent = date('date');
+objDOMs.addBtn.addEventListener('click', createTasks);
+objDOMs.input.addEventListener('keydown', createTasks);
+objDOMs.listContainer.addEventListener('click', checkedTasked);
+objDOMs.listContainer.addEventListener('click', deleteTasks);
+////////////////////////////////////////////////////////////////
+
 setInterval(function () {
+	objDOMs.dataInCircle.textContent = date('date');
 	objDOMs.TimeInCircle.textContent = date('timer');
 }, 1000);
 
 function date(where) {
-	let date = new Date();
-	let day = date.getDay();
-	let dateOftheMonth = date.getDate();
-	let month = date.getMonth();
-	let year = date.getFullYear();
-	let min = date.getMinutes();
-	let sec = date.getSeconds();
-	let hours = date.getHours();
+	let date, day, month, year, dateOftheMonth, min, sec, hours;
+
+	date = new Date();
+	day = date.getDay();
+	dateOftheMonth = date.getDate();
+	month = date.getMonth();
+	year = date.getFullYear();
+	min = date.getMinutes();
+	sec = date.getSeconds();
+	hours = date.getHours();
 
 	// console.log(date);
 	let days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
@@ -92,7 +128,7 @@ function date(where) {
 
 	let time = () => {
 		if (hours < 12) {
-			return `0${hours}:${min}:${sec} AM`;
+			return `${hours}:${min}:${sec} AM`;
 		} else {
 			return `${hours}:${min}:${sec} PM`;
 		}
@@ -112,6 +148,4 @@ function date(where) {
 }
 console.log(date('timer'));
 
-objDOMs.listContainer.addEventListener('click', checkedTasked);
-objDOMs.listContainer.addEventListener('click', deleteTasks);
-objDOMs.addBtn.addEventListener('click', createTasks);
+function scrollDown() {}
